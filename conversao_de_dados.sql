9c12a854147dcb0fd6f0f869358d6667ff63d89c
Felipi Okada <38229398+felipiokada@users.noreply.github.com>
2022-08-15 23:32:23 -0300

    Update conversao_de_dados.sql
    Adicionando mais especificadores de conversão como %c , %b, %d, %H, %i

--- conversao_de_dados.sql
+++ conversao_de_dados.sql
@@ -6,3 +6,9 @@ SELECT CONCAT('Ano do aniversário do cachorro: ', DATE_FORMAT(CURDATE(), '%Y'))
 SELECT CONCAT('Ano do aniversário do cachorro: ', DATE_FORMAT(CURDATE(), '%y')) AS `CACHORRO`;	/*os últimos dois digitos do ano atual*/
 SELECT CONCAT('Mês festivos da empresa: ', DATE_FORMAT(CURDATE(), '%M')) AS `FESTIVAIS`;	/*August*/
 SELECT CONCAT('Mês festivos da empresa: ', DATE_FORMAT(CURDATE(), '%m')) AS `FESTIVAIS`;	/*08*/
+
+SELECT CONCAT('Mês com mais tarefas no ano: ', DATE_FORMAT('1996-02-22', '%c')) AS `MÊS`;	/*2*/
+SELECT CONCAT('Mês com mais tarefas no ano: ', DATE_FORMAT('1996-02-22', '%b')) AS `MÊS`;	/*Feb*/
+SELECT CONCAT('Mês com mais tarefas no ano: ', DATE_FORMAT('1996-02-22', '%M')) AS `MÊS`;	/*February*/
+SELECT DATE_FORMAT(CURRENT_TIMESTAMP(), '%d/%m/%Y') AS `FORMATANDO A DATA EM DIA/MÊS/ANO`;
+SELECT CONCAT('Agora são: ', DATE_FORMAT(CURRENT_TIMESTAMP(), '%H:%i')) AS `FORMATANDO PARA HORAS`;
