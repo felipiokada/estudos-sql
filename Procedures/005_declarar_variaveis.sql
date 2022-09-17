d09bc534f07d5535f9e17d2a30cdcb03e736e2fb
Felipi Okada <38229398+felipiokada@users.noreply.github.com>
2022-09-16 23:40:34 -0300

    Update 005_declarar_variaveis.sql
    Adicionando informações sobre como declarar variáveis.

--- Procedures/005_declarar_variaveis.sql
+++ Procedures/005_declarar_variaveis.sql
@@ -1,1 +1,17 @@
 /* DECLARAR VARIÁVEIS NA STORED PROCEDURE */
+
+USE `sucos_vendas`;
+DROP PROCEDURE IF EXISTS `Atribuindo_valores`;
+
+DELIMITER $$
+CREATE PROCEDURE `Atribuindo_valores` ()
+BEGIN
+	DECLARE texto varchar(50) DEFAULT 'Nenhum valor';
+    DECLARE textoSemDefault varchar(50);				-- Quando não tem default, o valor atribuido é null
+    DECLARE dataDeCriacao DATE DEFAULT LOCALTIMESTAMP();
+    DECLARE numeroInteiro int(10) DEFAULT 1;
+    DECLARE numeroFlutuante double(20,4) DEFAULT 22.5;	-- (20, 4) 20 NÚMEROS sendo 4 para DEPOIS DA VÍRGULA
+    DECLARE palavra1, palavra2, palavra3 VARCHAR(15) DEFAULT 'Nenhum valor';	-- Pode declarar várias variáveis para o mesmo valor
+    SELECT texto, textoSemDefault, dataDeCriacao, numeroInteiro, numeroFlutuante, palavra1, palavra2, palavra3;
+END $$
+DELIMITER ;
